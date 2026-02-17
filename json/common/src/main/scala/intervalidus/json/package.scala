df8d7308f6d42eda0191eff8a799c2adaf1c2824
rremple <russ.remple@gmail.com>
2026-02-17 15:32:13 -0800

    package-level docs

---
+++ json/common/src/main/scala/intervalidus/json/package.scala
@@ -0,0 +1,7 @@
+package intervalidus
+
+/**
+  * Supports an example Intervalidus application 's sample JSON pickling subprojects which could be useful when managing
+  * Intervalidus data in a JSON data store (e.g., MongoDB) and/or serializing data through web services.
+  */
+package object json
---
+++ sidequests/tinyrule/src/main/scala/intervalidus/tinyrule/package.scala
@@ -0,0 +1,9 @@
+package intervalidus
+
+/**
+  * A toy rules engine which is not strictly related to Intervalidus, but it is used in an example rules-based
+  * application that shows how Intervalidus structures can be used to capture the outcomes of patients in a clinical
+  * trial. The toy rules engine is used to interpret daily measurements, and Intervalidus to capture progress concisely.
+  * It uses some interesting generic and metaprogramming features of Scala 3 to represent and process facts.
+  */
+package object tinyrule
